72e5d4f1cd47975c8515b5995a7402d267185597
NIMMALA SNEHITHA <164130486+SnehithaReddy1610@users.noreply.github.com>
2026-08-15 05:06:26 +0000

    conersion done

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,57 @@
+let score = "123"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => i, false => 0
+//"abc" => NaN
+
+
+let isLoggedIn = "hai"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+// " " => false
+// "hai" => true
+// 1 => true, 0 => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+let num = 10
+// console.log(typeof num);
+
+let StringNum = String(num)
+// console.log(StringNum)
+// console.log(typeof StringNum)
+
+let number = 0
+// console.log(typeof number)
+let isBool = Boolean(number)
+// console.log(isBool);
+
+
+
+
