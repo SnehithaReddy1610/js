cf003cb090fff11161855cc6987e0a01dc462916
NIMMALA SNEHITHA <164130486+SnehithaReddy1610@users.noreply.github.com>
2026-08-16 12:52:00 +0000

    string and maths

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,87 @@
+const name = "hello" 
+const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String (`snehitha_ns_reddy`)
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('t'));
+
+
+// const newString = gameName.substring(0,7)  // if we take here neg values it will ignore and start from 0
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 7)
+// console.log(anotherString);
+
+// const newStrinOne = "     ammu     "
+// console.log(newStrinOne);
+// console.log(newStrinOne.trim());
+
+const url = "https://snehitha.com/snehitha%16reddy"
+
+// console.log(url.replace('%16', '-'));
+
+// console.log(url.includes('sundar'));
+
+// console.log(gameName.split('_'));
+
+
+
+
+
+
+/***************NUMBERS AND MATHS******************* */
+
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toExponential());
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));  
+
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++MATHS+++++++++++++++++++++++///
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.sqrt(25));
+// console.log(Math.min(4,3,2,5,6));
+// console.log(Math.max(4,3,2,5,6));
+console.log(Math.random());
+console.log((Math.floor(Math.random() * 10 )+ 1));
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
